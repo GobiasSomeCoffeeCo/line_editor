1950961bbcac9e85da3f85f91edc838d6a00a612
GobiasSomeCoffeeCo <60927833+GobiasSomeCoffeeCo@users.noreply.github.com>
2021-07-13 22:52:04 -0400

    Line Editor w/ JT Part 1

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,29 @@ use std::{
 };
 
 use crossterm::{
+    cursor::MoveLeft,
     event::DisableMouseCapture,
     event::Event,
     event::{read, KeyEvent},
     event::{EnableMouseCapture, KeyCode},
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    terminal, ExecutableCommand, Result,
+    terminal, ExecutableCommand, QueueableCommand, Result,
 };
 
 fn main() -> Result<()> {
+    let mut stdout = stdout();
     let mut buffer = String::new();
     // using the macro
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        Print("> "),
-        ResetColor,
-    )?;
+    // execute!(
+    //     stdout(),
+    //     SetForegroundColor(Color::Blue),
+    //     Print("> "),
+    //     ResetColor,
+    // )?;
 
     // or using functions
-    stdout()
+    stdout
         .execute(SetForegroundColor(Color::Blue))?
         .execute(Print("> "))?
         .execute(ResetColor)?;
@@ -36,11 +38,26 @@ fn main() -> Result<()> {
             Event::Key(KeyEvent { code, modifiers }) => {
                 match code {
                     KeyCode::Char(c) => {
-                        let mut char_buffer = [0; 4];
-                        let bytes = c.encode_utf8(&mut char_buffer).as_bytes();
-                        stdout().write_all(&bytes)?;
-                        stdout().flush()?;
+                        // let mut char_buffer = [0; 4];
+                        // let bytes = c.encode_utf8(&mut char_buffer).as_bytes();
+                        // stdout.write_all(&bytes)?;
+                        // stdout.flush()?;
+                        stdout.queue(Print(c))?;
+                        stdout.flush()?;
                         buffer.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        if !buffer.is_empty() {
+                            buffer.pop();
+                            stdout
+                                .queue(MoveLeft(1))?
+                                .queue(Print(" "))?
+                                .queue(MoveLeft(1))?;
+                            // stdout.execute(MoveLeft(1))?;
+                            // stdout.write_all(b" ")?;
+                            // stdout.execute(MoveLeft(1))?;
+                            stdout.flush()?;
+                        }
                     }
                     KeyCode::Enter => {
                         break;
@@ -56,7 +73,7 @@ fn main() -> Result<()> {
             }
         }
     }
-
+    println!("Our buffer: {}", buffer);
     terminal::disable_raw_mode()?;
 
     Ok(())
